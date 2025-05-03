e41501fba732d89c748c12b38338963d19e56b5d
Vlad Opris <163576376+vld2405@users.noreply.github.com>
2025-05-03 19:57:24 +0300

    project uploaded

---
+++ CS2Stats.Infrastructure/Config/AppConfig.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using CS2Stats.Infrastructure.Config.Models;
+
+namespace CS2Stats.Infrastructure.Config
+{
+    public class AppConfig
+    {
+        public static bool ConsoleLogQueries = true;
+        public static ConnectionStringsSettings? ConnectionStrings { get; set; }
+
+        public static void Init(IConfiguration configuration)
+        {
+            Configure(configuration);
+        }
+
+        private static void Configure(IConfiguration configuration)
+        {
+            ConnectionStrings = configuration.GetSection("ConnectionStrings").Get<ConnectionStringsSettings>();
+        }
+    }
+}
